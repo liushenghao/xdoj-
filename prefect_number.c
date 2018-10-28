fc053a36bf8f919a382f1dbd497b2e9d85cc5934
1042686202@qq.com <1042686202@qq.com>
2018-10-28 15:47:02 +0800

    attention! the algorithms

---
+++ prefect_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	int i,j;
+	int sum=0;
+	scanf("%d %d",&a,&b);
+	for(i=a;i<=b;i++)
+	{
+		for(j=1;j<i;j++)
+		{
+			if(i%j==0)
+			{
+				sum=sum+j;
+			}
+		}
+		if(sum==i)
+		{
+			printf("%d\n",i);
+		}
+		sum=0; 
+	}
+	
+	return 0;
+} 
